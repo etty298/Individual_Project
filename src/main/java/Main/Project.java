5522a5cd00ed4fb13442a361ef6f2562e8a3bcab
Матвей <etty298@gmail.com>
2022-04-18 21:52:40 +0300

    Commit Message

--- src/main/java/Main/Project.java
+++ src/main/java/Main/Project.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Project extends TelegramLongPollingBot {
-    private static final String TOKEN = "5221396288:AAHpNNFjLkmtv_jHFpLR7Z9RmZEu3PbsBck";
+    private static final String TOKEN = "5021269353:AAEZHqSsfUJ0wMwlZ_hHrrAU4Tjv0tZvhXU";
     private static final String USERNAME = "Project_366_Bot";
     public String state;
 
@@ -37,7 +37,7 @@ public class Project extends TelegramLongPollingBot {
                 case "/start":
                     sendMessage(update.getMessage(), "Вы можете найти подходящуюю для вас тему проекта");
                     break;
-                case "Выбрать предмет":
+                case "Выбрать тип проекта":
                     sendTypeMessage(update.getMessage(), "Выберите тип проекта");
                     break;
                 case "Теоретический":
@@ -51,14 +51,14 @@ public class Project extends TelegramLongPollingBot {
                 case "Информатика":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по информатике:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Киберпреступность и методы борьбы с ней\n" +
+                                "Польза и вред компьютерных игр\n" +
+                                "Эволюция операционной системы\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по информатике:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Создание тематического web-сайта\n" +
+                                "Создание собственной компьютерной игры\n" +
+                                "Распознавание объектов с помощью нейронных сетей\n");
                     }
 
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
@@ -66,14 +66,14 @@ public class Project extends TelegramLongPollingBot {
                 case "Математика":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по математике:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Экология глазами математики\n" +
+                                "Параболы в арочных мостах\n" +
+                                "Вероятность сдачи ОГЭ\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по математике:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Методы решения уравнений 4 степени\n" +
+                                "Различные способы доказательства теоремы Пифагора\n" +
+                                "Равносильные преобразования неравенств: теория и практика.\n");
                     }
 
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
@@ -81,14 +81,14 @@ public class Project extends TelegramLongPollingBot {
                 case "История":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по истории:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Духовная жизнь в 20-е годы 20 века\n" +
+                                "Геополитическое положение и внешняя политика России\n" +
+                                "Политическая система СССР в 30-е годы 20 века\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по истории:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Перестройка в СССР 1985-1991гг\n" +
+                                "Назад в СССР\n" +
+                                "Проект реформ П.А. Столыпина\n");
                     }
 
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
@@ -96,15 +96,15 @@ public class Project extends TelegramLongPollingBot {
                 case "География":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по географии:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Антропогенные катастрофы в природе\n" +
+                                "Есть ли аномалии тяжелых металлов вдоль автомобильных магистра лей нашего города? Их влияние на наше здоровье\n" +
+                                "Древние корни молодого города\n");
 
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по географии:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Жизнь на горячей земле\n" +
+                                "Историко-геологическая экскурсия в центре города.\n" +
+                                "Воздействие человека на климат\n");
 
                     }
 
@@ -113,51 +113,51 @@ public class Project extends TelegramLongPollingBot {
                 case "Биология":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по биологии:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Акустический шум и его воздействие на человека\n" +
+                                "Биологический механизм запахов\n" +
+                                "Паразитирующие жгутиконосцы\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по биологии:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Влияние почв на растения\n" +
+                                "Модификационная изменчивость бездомного котенка\n" +
+                                "Практические аспекты взаимодействия между людьми и птицами\n");
                     }
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
                     break;
                 case "Русский язык":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по русскому языку:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Источник пополнения словарного запаса русского языка\n" +
+                                "Источники крылатых слов и выражений\n" +
+                                "Штампы и стереотипы в современной публичной речи\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по русскому языку:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Лексикон учащихся 9-х классов моей школы\n" +
+                                "Стилистическое использование профессиональной и терминологической лексики в произведениях художественной литературы\n" +
+                                "Слова-паразиты в речи учителей и учащихся\n");
                     }
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
                     break;
                 case "Литература":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по литературе:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Анализ исследование одного стихотворения\n" +
+                                "Влияние творчества Дж. Байрона на русскую классическую литературу\n" +
+                                "Влияние нецензурной и обсценной лексики на человека\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по литературе:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Экскурсия по пушкинским местам\n" +
+                                "Создание электронного методического пособия по литературе по изучению творчества С.А. Есенина в школе\n" +
+                                "Сопоставление поэтических переводов «Слова…»\n");
                     }
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
                     break;
                 case "Иностранный язык":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по иностранному языку:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Глобализация английского языка и его влияние на русский язык\n" +
+                                "Исторические связи России и Великобритании\n" +
+                                "Начальная школа в Британии\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по иностранному языку:\n" +
                                 "-\n" +
@@ -170,56 +170,56 @@ public class Project extends TelegramLongPollingBot {
                 case "Физика":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по физике:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Влияние температуры окружающей среды на изменение снежных узоров на оконном стекле\n" +
+                                "Влияние радиоактивности на окружающую среду\n" +
+                                "Влияние излучения, исходящего от сотового телефона, на организм человека\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по физике:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Взвешивание воздуха\n" +
+                                "Определение содержания кислорода в воздухе\n" +
+                                "Изобретение электрического стула\n");
                     }
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
                     break;
                 case "Обществознание":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по оюществознанию:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "История появления Конституции РФ\n" +
+                                "Виды наций и межнациональные взаимоотношения\n" +
+                                "Значение технического прогресса в жизни общества\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по оюществознанию:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Участие гражданина в делаx государства\n" +
+                                "Создание бизнес плана\n" +
+                                "Социальные сети: инструмент для лучшей организации повседневности или средство развлечения?\n");
                     }
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
                     break;
                 case "Химия":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по химии:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Металлы – материал для создания шедевров мирового искусства\n" +
+                                "Кислотные осадки: их природа и последствия\n" +
+                                "География химических названий\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по химии:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Анализ проб воды и воздуха в различных частях города\n" +
+                                "Гидролиз солей\n" +
+                                "Исследование орехов миндаля на содержание цианид-ионов\n");
                     }
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
                     break;
                 case "Физическая культура":
                     if (state.equals("Теоретический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для теоретических проектов по физической культуре:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Значение ведения самоконтроля при занятиях физическими упражнениями\n" +
+                                "Мир спорта моими глазами\n" +
+                                "Развитие физических возможностей путем самостоятельной работы\n");
                     } else if (state.equals("Практический")) {
                         sendLessonMessage(update.getMessage(), "Возможные варианты для практических проектов по физической культуре:\n" +
-                                "-\n" +
-                                "-\n" +
-                                "-\n");
+                                "Как самостоятельно построить спортивную площадку\n" +
+                                "От чего зависит частота пульса?\n" +
+                                "Путь к Олимпу. Мой дневник достижений.\n");
                     }
                     sendLessonMessage(update.getMessage(), "Выберите подходящий предмет из списка");
                     break;
@@ -272,7 +272,7 @@ public class Project extends TelegramLongPollingBot {
 
         List<KeyboardRow> keyboardRowList = new ArrayList<>();
         KeyboardRow keyboardFirstRow = new KeyboardRow();
-        keyboardFirstRow.add(new KeyboardButton("Выбрать предмет"));
+        keyboardFirstRow.add(new KeyboardButton("Выбрать тип проекта"));
         keyboardRowList.add(keyboardFirstRow);
         replyKeyboardMarkup.setKeyboard(keyboardRowList);
 
@@ -304,6 +304,7 @@ public class Project extends TelegramLongPollingBot {
         KeyboardRow keyboardSecondRow = new KeyboardRow();
         KeyboardRow keyboardThirdRow = new KeyboardRow();
         KeyboardRow keyboardFourthRow = new KeyboardRow();
+        KeyboardRow keyboardFifthRow = new KeyboardRow();
         keyboardFirstRow.add(new KeyboardButton("Информатика"));
         keyboardFirstRow.add(new KeyboardButton("Математика"));
         keyboardFirstRow.add(new KeyboardButton("История"));
@@ -316,10 +317,12 @@ public class Project extends TelegramLongPollingBot {
         keyboardFourthRow.add(new KeyboardButton("Обществознание"));
         keyboardFourthRow.add(new KeyboardButton("Химия"));
         keyboardFourthRow.add(new KeyboardButton("Физическая культура"));
+        keyboardFifthRow.add(new KeyboardButton("Выбрать тип проекта"));
         keyboardRowList.add(keyboardFirstRow);
         keyboardRowList.add(keyboardSecondRow);
         keyboardRowList.add(keyboardThirdRow);
         keyboardRowList.add(keyboardFourthRow);
+        keyboardRowList.add(keyboardFifthRow);
         replyKeyboardMarkup.setKeyboard(keyboardRowList);
 
     }
